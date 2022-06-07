283f4a79835fd4b1e86835f708ac09076067d749
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-06-07 20:00:02 +0600

    [+]Added main class

---
+++ src/main/java/com/me/testtask/Main.java
@@ -0,0 +1,15 @@
+package com.me.testtask;
+
+/**
+ * Application starting class
+ */
+public class Main {
+    /**
+     * Application starting method
+     *
+     * @param args given arguments to application(will not be used)
+     */
+    public static void main(String[] args) {
+
+    }
+}
