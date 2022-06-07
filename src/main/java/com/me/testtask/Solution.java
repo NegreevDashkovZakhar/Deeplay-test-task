2fa812215025f96d156b1b47f515a4dd9e9e5fc2
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2022-06-07 20:12:07 +0600

    [+]Added empty solution class

---
+++ src/main/java/com/me/testtask/Solution.java
@@ -0,0 +1,18 @@
+package com.me.testtask;
+
+/**
+ * Class responsible for whole task
+ */
+public class Solution {
+    /**
+     * Method calculating closest path from top left corner of given map to bottom right corner
+     *
+     * @param map          string representing 4x4 grid where the path should be found. Must contain 16 symbols
+     * @param creatureName string representing creature name walking the path
+     * @return minimal cost of moving specified creature from top left corner to bottom right corner of the specified
+     * grid
+     */
+    public static int getResult(final String map, final String creatureName) {
+        return 0;
+    }
+}
